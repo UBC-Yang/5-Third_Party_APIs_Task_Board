7525ef31586a61141918b63d04035827b54e5b18
Shu Yang <shuyang@Thomass-MacBook-Pro.local>
2024-05-15 19:57:19 -0600

    delete btn added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,11 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
-let nextId = JSON.parse(localStorage.getItem("nextId"));
+let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+let nextId = JSON.parse(localStorage.getItem("nextId")) || 1;
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-    if (!nextId) {
-        nextId = 1;
-    } else {
-        nextId++
-    }
+    const Id = nextId;
+    nextId++;
     localStorage.setItem('nextId', JSON.stringify(nextId));
     return nextId
 }
@@ -19,10 +16,16 @@ function createTaskCard(task) {
     const dueDate = $('<h6>').addClass('card-dueDate').text(task.dueDate);
     const description = $('<p>').addClass('card-text').text(task.description);
 
+    const deleteButton = $('<button>')
+        .addClass('btn btn-danger btn-sm')
+        .text('Delete')
+        .click(handleDeleteTask);
+
     const cardBody = $('<div>').addClass('card-body')
         .append(title)
         .append(dueDate)
-        .append(description);
+        .append(description)
+        .append(deleteButton);
 
     const card = $('<div>')
         .addClass('card mb-3')
@@ -32,8 +35,6 @@ function createTaskCard(task) {
     return card;
 }
 
-$('#addTaskButton').on('click', createTaskCard)
-
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
     const lanes = {
@@ -41,6 +42,15 @@ function renderTaskList() {
         'in-progress': $('#in-progress-cards'),
         'done': $('#done-cards')
     }
+
+    for (let lane in lanes) {
+        lanes[lane].empty();
+    }
+
+    taskList.forEach(task => {
+        const taskCard = createTaskCard(task);
+        lanes[task.status].append(taskCard);
+    });
 }
 
 // Todo: create a function to handle adding a new task
@@ -59,6 +69,7 @@ function handleAddTask(event){
         status: 'to-do'
     }
 
+    taskList.push(newTask);
     localStorage.setItem('tasks', JSON.stringify(taskList));
 
     renderTaskList();
@@ -69,12 +80,13 @@ function handleAddTask(event){
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
-
+    const card = (event.target || event.srcElement).closest(".card");
+    card.remove()
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
-
+    
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
